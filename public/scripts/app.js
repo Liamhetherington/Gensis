c49e84f707315d1717289b1fdc35cfe5a47235d6
Liam Hetherington <liambhetherington@gmail.com>
2019-05-24 00:47:15 +0000

    pulling from master

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,14 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
-});
+function createResourceElement(resource) {
+  let $resource = $('<article>');
+  let $footer = $('<footer>');
+  let $content = $('<div>').attr('src', resource.url);
+  return $content
+}
+
+
+
+
+$(document).ready(function() {
+  createResourceElement();
+  // console.log("ready")
+})
