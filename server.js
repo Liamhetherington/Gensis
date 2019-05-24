a0f6f5fdd77e3ef577d5f6f0d4920bda59628cf8
vagrant <you@example.com>
2019-05-24 22:56:23 +0000

    added new table column, resolved conflict with user-cookies

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const sass = require("node-sass-middleware");
 const app = express();
-const cookieSession = require('cookie-session');
-const bcrypt = require('bcrypt');
+const cookieSession = require("cookie-session");
+const bcrypt = require("bcrypt");
 
 const knexConfig = require("./knexfile");
 const knex = require("knex")(knexConfig[ENV]);
@@ -24,10 +24,12 @@ const usersRoutes = require("./routes/users");
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
 
-app.use(cookieSession({
-    name: 'session',
-    keys: ["1"],
-}))
+app.use(
+	cookieSession({
+		name: "session",
+		keys: ["1"]
+	})
+);
 
 // Log knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
@@ -51,63 +53,64 @@ app.use("/api/users", usersRoutes(knex));
 // Home page
 
 app.get("/", (req, res) => {
-   if (req.session.id === undefined) {
-        return res.render("index", {username: ""});
-    }
-    knex.select("username").from("users").where('id',req.session.id)
-    .then(function (result){
-    let templateVars = {username: result[0].username};
-    res.render("index", templateVars);
-    })
+	if (req.session.id === undefined) {
+		return res.render("index", { username: "" });
+	}
+	knex.select("username")
+		.from("users")
+		.where("id", req.session.id)
+		.then(function(result) {
+			let templateVars = { username: result[0].username };
+			res.render("index", templateVars);
+		});
 });
-
-
 
 app.get("/login", (req, res) => {
-    res.render("login");
+	res.render("login");
 });
 
-
 app.post("/login", (req, res) => {
-
 	let result = checkUsername(req.body.username);
- 	result.then((value)=>{
-   if(value > 0){
-    req.session.id = value;
-   	res.redirect("/")
+	result.then(value => {
+		if (value > 0) {
+			req.session.id = value;
+			res.redirect("/");
 
-    console.log("user found in the database with id ", value);
-   } else{
-   	res.send("Username not found");
-   }
+			console.log("user found in the database with id ", value);
+		} else {
+			res.send("Username not found");
+		}
 	});
 });
 
 //function to check if the user is existed in database
-function checkUsername(username){
- return knex.select("id").from("users").where('username',username)
- .then(function (users){
-   if(users.length>0){
-     return Promise.resolve(users[0].id);
-   } else {
-     return Promise.resolve(0)
-   }
-   console.log("its after knex query");
- });
+function checkUsername(username) {
+	return knex
+		.select("id")
+		.from("users")
+		.where("username", username)
+		.then(function(users) {
+			if (users.length > 0) {
+				return Promise.resolve(users[0].id);
+			} else {
+				return Promise.resolve(0);
+			}
+			console.log("its after knex query");
+		});
 }
-
-
-
-
-
-app.post("/logout", (req, res) => {
-	  req.session = null;
-    res.redirect("/");
-});
 
 // new page
 app.get("/new", (req, res) => {
-	res.render("newResource");
+	if (req.session.id === undefined) {
+		return res.render("index", { username: "" });
+	}
+	knex.select("username")
+		.from("users")
+		.where("id", req.session.id)
+		.then(function(result) {
+			let templateVars = { username: result[0].username };
+			res.render("newResource", templateVars);
+		});
 });
 
 app.post("/new", (req, res) => {
@@ -117,7 +120,8 @@ app.post("/new", (req, res) => {
 			title: req.body.title,
 			url: req.body.source_url,
 			// date_created : "5/23/2019",
-			description: req.body.description
+			description: req.body.description,
+			users_id: req.session.id
 		})
 		.then(function(result) {
 			res.redirect("/genesis");
@@ -125,11 +129,29 @@ app.post("/new", (req, res) => {
 });
 
 app.get("/genesis", (req, res) => {
-	const username = req.body.username
-	res.render("myResources");
+	if (req.session.id === undefined) {
+		return res.render("index", { username: "" });
+	}
+	knex.select("username")
+		.from("users")
+		.where("id", req.session.id)
+		.then(function(result) {
+			let templateVars = { username: result[0].username };
+			res.render("myResources", templateVars);
+		});
 });
 
 app.post("/genesis", (req, res) => {
+	if (req.session.id === undefined) {
+		return res.render("index", { username: "" });
+	}
+	knex.select("username")
+		.from("users")
+		.where("id", req.session.id)
+		.then(function(result) {
+			let templateVars = { username: result[0].username };
+			res.render("myResources", templateVars);
+		});
 	res.redirect("/new");
 });
 
@@ -139,12 +161,9 @@ app.get("/info", (req, res) => {
 });
 
 app.get("/resource", (req, res) => {
-	knex("resource")
-		.first()
-		.then(resource => {
-			console.log("resource: ", resource);
-			return res.json(resource);
-		});
+	knex("resource").then(resource => {
+		return res.json(resource);
+	});
 	// res.render("info");
 });
 app.get("/comments", (req, res) => {
@@ -183,11 +202,9 @@ app.post("/rating", (req, res) => {
 	// resource_id
 });
 
-app.get("/resource", (req, res) => {
-	knex("resource").then(resource => {
-		return res.json(resource);
-	});
-	// res.render("info");
+app.post("/logout", (req, res) => {
+	req.session = null;
+	res.redirect("/");
 });
 
 app.listen(PORT, () => {
