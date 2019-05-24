6f637eefe78602533a517f01f463b6192a2b4a55
Liam Hetherington <liambhetherington@gmail.com>
2019-05-24 20:18:11 +0000

    addjust function to render resource elements on my info page. adjust css styling for my info page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,35 @@
 function createResourceElement(resource) {
   let $title = $('#resource-title');
     $title.text(resource.title);
-  let $object= $('<object>');
-  let $footer = $('<footer>');
-  let $content = $('<div>').attr('src', resource.url);
-  console.log(resource.url);
-  return $content
+  // let $object= $('#resource-media');
+  //   $object.find(resource.url);
+  let $description = $('#description');
+    $description.text(resource.description);
+  let $date = $('#date');
+    $date.text(resource.date_created);
+  console.log("description: ", resource.desciption);
 }
 
+function renderResource(resource) {
+  $('<body>').empty();
+  let $currentResource = $('<body>');
+  resource.forEach(function (resourceInfo) {
+    let $resourceInfo = createResourceElement(resourceInfo)
+    $currentResource.append($resourceInfo)
+  })
+}
+
+function loadResource() {
+  $.ajax({
+    type:"GET",
+    url:'/resource',
+    data: JSON,
+    success: function (data) {
+      renderResource(data)
+    }
+  })
+}
+loadResource();
 
 $(document).ready(function() {
   $.ajax({
@@ -18,3 +40,4 @@ $(document).ready(function() {
     }
   });
 })
+
