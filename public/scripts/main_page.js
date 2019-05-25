a98fe32aa52706ba03194bea099970e2f89ae86b
vagrant <you@example.com>
2019-05-25 20:38:00 +0000

    resolving merge conflict

--- public/scripts/main_page.js
+++ public/scripts/main_page.js
@@ -1,39 +1,41 @@
-function createMainDisplay(resource) {
-	let $thumbnail = $("<img>");
-	$thumbnail.attr("src", resource.thumbnail);
-	$thumbnail.addClass("thumbnailImage");
-	return $thumbnail;
-}
-
-function renderImages(resource) {
-	let $thumbnails = $("#thumbnails");
-	console.log($thumbnails);
-	console.log(resource);
-	resource.forEach(function(images) {
-		let $imageRender = createMainDisplay(images);
-		console.log($imageRender);
-		$thumbnails.append($imageRender);
-	});
-}
-
-function loadThumbnails() {
-	$.ajax({
-		type: "GET",
-		url: "/resource",
-		data: JSON,
-		success: function(data) {
-			renderImages(data);
-		}
-	});
-}
-loadThumbnails();
+// loadThumbnails();
 
 $(document).ready(function() {
-	$.ajax({
-		type: "GET",
-		url: "/resource",
-		success: resource => {
-			createMainDisplay(resource);
-		}
-	});
+	loadThumbnails();
+	// $.ajax({
+	// 	type: "GET",
+	// 	url: "/resource",
+	// 	success: resource => {
+	// 		createMainDisplay(resource);
+	// 	}
+	// });
+
+	function createMainDisplay(resource) {
+		let $thumbnail = $("<img>");
+		$thumbnail.attr("src", resource.thumbnail);
+		$thumbnail.addClass("thumbnailImage");
+		return $thumbnail;
+	}
+
+	function renderImages(resource) {
+		let $thumbnails = $("#thumbnails");
+		console.log($thumbnails);
+		console.log(resource);
+		resource.forEach(function(images) {
+			let $imageRender = createMainDisplay(images);
+			console.log($imageRender);
+			$thumbnails.append($imageRender);
+		});
+	}
+
+	function loadThumbnails() {
+		$.ajax({
+			type: "GET",
+			url: "/resource",
+			data: JSON,
+			success: function(data) {
+				renderImages(data);
+			}
+		});
+	}
 });
