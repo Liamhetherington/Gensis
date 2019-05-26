e43fa475e59fecde91df26e6178a8d1fc30f6e25
sunasunasun <xiaoyu.sun@ucalgary.ca>
2019-05-26 18:47:19 +0000

    load image on my resource page

--- public/scripts/categorize.js
+++ public/scripts/categorize.js
@@ -1,36 +1,82 @@
-jQuery.fn.filterByText = function(textbox) {
-  return this.each(function() {
-    var select = this;
-    var options = [];
-    $(select).find('option').each(function() {
-      options.push({
-        value: $(this).val(),
-        text: $(this).text()
-      });
+$(document).ready(function() {
+  loadThumbnails();
+
+
+  function createMainDisplay(resource) {
+    let $thumbnail = $("<img>");
+    $thumbnail.attr("src", resource.thumbnail);
+    $thumbnail.addClass("thumbnailImage");
+    return $thumbnail;
+  }
+
+  function renderImages(resource) {
+    let $thumbnails = $("#thumbnails");
+    console.log($thumbnails);
+    console.log(resource);
+    resource.forEach(function(images) {
+      let $imageRender = createMainDisplay(images);
+      console.log($imageRender);
+      $thumbnails.append($imageRender);
     });
-    $(select).data('options', options);
+  }
 
-    $(textbox).bind('change keyup', function() {
-      var options = $(select).empty().data('options');
-      var search = $.trim($(this).val());
-      var regex = new RegExp(search, "gi");
-
-      $.each(options, function(i) {
-        var option = options[i];
-        if (option.text.match(regex) !== null) {
-          $(select).append(
-            $('<option>').text(option.text).val(option.value)
-          );
-        }
-      });
+  function loadThumbnails() {
+    $.ajax({
+      type: "GET",
+      url: "/resource",
+      data: JSON,
+      success: function(data) {
+        renderImages(data);
+      }
     });
-  });
-};
-
-
-$(function() {
-  $('select').filterByText($('input'));
+  }
 });
 
 
-*every time choose a topic, empty container(twetter)
+
+
+
+
+
+
+
+
+
+
+
+// jQuery.fn.filterByText = function(textbox) {
+//   return this.each(function() {
+//     var select = this;
+//     var options = [];
+//     $(select).find('option').each(function() {
+//       options.push({
+//         value: $(this).val(),
+//         text: $(this).text()
+//       });
+//     });
+//     $(select).data('options', options);
+
+//     $(textbox).bind('change keyup', function() {
+//       var options = $(select).empty().data('options');
+//       var search = $.trim($(this).val());
+//       var regex = new RegExp(search, "gi");
+
+//       $.each(options, function(i) {
+//         var option = options[i];
+//         if (option.text.match(regex) !== null) {
+//           $(select).append(
+//             $('<option>').text(option.text).val(option.value)
+//           );
+//         }
+//       });
+//     });
+//   });
+// };
+
+
+// $(function() {
+//   $('select').filterByText($('input'));
+// });
+
+
+// *every time choose a topic, empty container(twetter)
